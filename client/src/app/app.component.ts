8c94e11e95b76bdb7474feaa18acbebd975592fb
seanhuang <sean.huang@unisys.com>
2020-11-03 23:54:41 -0400

    Day3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating Application';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+
+  }
+
+  ngOnInit() {
+
+    this.getUsers();
+
+  }
+
+  getUsers() {
+
+    this.http.get('https://localhost:5001/api/users').subscribe(Response => {
+
+      this.users = Response;
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
 }
